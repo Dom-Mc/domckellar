18a3431c426ea0b145422fb14e7580de056fa5ee
Dom McKellar <dom.mckellar@gmail.com>
2017-01-28 15:32:33 -0800

    Uncomment ajax call and remove debuggers

--- js/app.js
+++ js/app.js
@@ -14505,13 +14505,13 @@ jQuery.extend( jQuery.easing,
 })( jQuery );
 
 $(function() {
-debugger
+// debugger
     $("#contactForm input, #contactForm textarea").jqBootstrapValidation({
         preventSubmit: true,
         submitError: function($form, event, errors) {
             // additional error messages or events
         },
-        debugger
+        // debugger
         submitSuccess: function($form, event) {
 
             // Prevent spam click and default submit behaviour
@@ -14519,7 +14519,7 @@ debugger
             event.preventDefault();
 
 
-            debugger
+            // debugger
 
             // NOTE: get values from FORM
             var name = $("input#name").val();
@@ -14532,47 +14532,47 @@ debugger
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
 
-    //         $.ajax({
-    //             url: 'https://formspree.io/email.domckellar@gmail.com',
-    //             method: "POST",
-    //             data: {
-    //                 name: name,
-    //                 phone: phone,
-    //                 email: email,
-    //                 message: message
-    //             },
-    //             dataType: "json"
-    //             // cache: false,
-    //             success: function() {
-    //                 // Enable button & show success message
-    //                 $("#btnSubmit").attr("disabled", false);
-    //                 $('#success').html("<div class='alert alert-success'>");
-    //                 $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-    //                     .append("</button>");
-    //                 $('#success > .alert-success')
-    //                     .append("<strong>Your message has been sent. </strong>");
-    //                 $('#success > .alert-success')
-    //                     .append('</div>');
-    //
-    //                 //clear all fields
-    //                 $('#contactForm').trigger("reset");
-    //             },
-    //             error: function() {
-    //                 // Fail message
-    //                 $('#success').html("<div class='alert alert-danger'>");
-    //                 $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-    //                     .append("</button>");
-    //                 $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-    //                 $('#success > .alert-danger').append('</div>');
-    //                 //clear all fields
-    //                 $('#contactForm').trigger("reset");
-    //             },
-    //         });
-    //     },
-    //     filter: function() {
-    //         return $(this).is(":visible");
-    //     },
-    // });
+            $.ajax({
+                url: 'https://formspree.io/email.domckellar@gmail.com',
+                method: "POST",
+                data: {
+                    name: name,
+                    phone: phone,
+                    email: email,
+                    message: message
+                },
+                dataType: "json",
+                // cache: false,
+                success: function() {
+                    // Enable button & show success message
+                    $("#btnSubmit").attr("disabled", false);
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-success')
+                        .append("<strong>Your message has been sent. </strong>");
+                    $('#success > .alert-success')
+                        .append('</div>');
+
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
+                error: function() {
+                    // Fail message
+                    $('#success').html("<div class='alert alert-danger'>");
+                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append('</div>');
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
+            });
+        },
+        filter: function() {
+            return $(this).is(":visible");
+        },
+    });
 
     $("a[data-toggle=\"tab\"]").click(function(e) {
         e.preventDefault();
--- js/contact_me.js
+++ js/contact_me.js
@@ -1,11 +1,11 @@
 $(function() {
-debugger
+// debugger
     $("#contactForm input, #contactForm textarea").jqBootstrapValidation({
         preventSubmit: true,
         submitError: function($form, event, errors) {
             // additional error messages or events
         },
-        debugger
+        // debugger
         submitSuccess: function($form, event) {
 
             // Prevent spam click and default submit behaviour
@@ -13,7 +13,7 @@ debugger
             event.preventDefault();
 
 
-            debugger
+            // debugger
 
             // NOTE: get values from FORM
             var name = $("input#name").val();
@@ -26,47 +26,47 @@ debugger
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
 
-    //         $.ajax({
-    //             url: 'https://formspree.io/email.domckellar@gmail.com',
-    //             method: "POST",
-    //             data: {
-    //                 name: name,
-    //                 phone: phone,
-    //                 email: email,
-    //                 message: message
-    //             },
-    //             dataType: "json"
-    //             // cache: false,
-    //             success: function() {
-    //                 // Enable button & show success message
-    //                 $("#btnSubmit").attr("disabled", false);
-    //                 $('#success').html("<div class='alert alert-success'>");
-    //                 $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-    //                     .append("</button>");
-    //                 $('#success > .alert-success')
-    //                     .append("<strong>Your message has been sent. </strong>");
-    //                 $('#success > .alert-success')
-    //                     .append('</div>');
-    //
-    //                 //clear all fields
-    //                 $('#contactForm').trigger("reset");
-    //             },
-    //             error: function() {
-    //                 // Fail message
-    //                 $('#success').html("<div class='alert alert-danger'>");
-    //                 $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-    //                     .append("</button>");
-    //                 $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-    //                 $('#success > .alert-danger').append('</div>');
-    //                 //clear all fields
-    //                 $('#contactForm').trigger("reset");
-    //             },
-    //         });
-    //     },
-    //     filter: function() {
-    //         return $(this).is(":visible");
-    //     },
-    // });
+            $.ajax({
+                url: 'https://formspree.io/email.domckellar@gmail.com',
+                method: "POST",
+                data: {
+                    name: name,
+                    phone: phone,
+                    email: email,
+                    message: message
+                },
+                dataType: "json",
+                // cache: false,
+                success: function() {
+                    // Enable button & show success message
+                    $("#btnSubmit").attr("disabled", false);
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-success')
+                        .append("<strong>Your message has been sent. </strong>");
+                    $('#success > .alert-success')
+                        .append('</div>');
+
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
+                error: function() {
+                    // Fail message
+                    $('#success').html("<div class='alert alert-danger'>");
+                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append('</div>');
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
+            });
+        },
+        filter: function() {
+            return $(this).is(":visible");
+        },
+    });
 
     $("a[data-toggle=\"tab\"]").click(function(e) {
         e.preventDefault();
